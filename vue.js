b401fcb356049ab19643d6dfe6c89c2b650306b9
marco-contreras <marco.contreras@gmail.com>
2018-05-03 12:42:43 -0700

    Call to DataVars and Methods

--- vue.js
+++ vue.js
@@ -2,5 +2,14 @@ var app = new Vue({
     el: '.vue',
     data: {
         message: 'Aprende Vue.js Facilmente'
+    },
+    methods: {
+        showMessage: function(){
+            return 'Lern Vue';
+        },
+        showFirstMessage: function(){
+            return this.message;
+        }
     }
+
 });
