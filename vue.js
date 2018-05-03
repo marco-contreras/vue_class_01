5774178659b56d86751a02019349e235b690f02b
marco-contreras <marco.contreras@gmail.com>
2018-05-03 14:31:03 -0700

    Computed properties, Update on real time

--- vue.js
+++ vue.js
@@ -2,7 +2,16 @@ var app = new Vue({
     el: '.vue',
     data: {
         message: 'Aprende Vue.js Facilmente',
-        src: 'https://steemit-production-imageproxy-upload.s3.amazonaws.com/DQmReM2aoZysGbehUrN6RWkRfRLjaVuR3xL3XFpiY17x7QB'
+        src: 'https://steemit-production-imageproxy-upload.s3.amazonaws.com/DQmReM2aoZysGbehUrN6RWkRfRLjaVuR3xL3XFpiY17x7QB',
+        val_1: 0,
+        val_2: 0,
+        val_3: 0,
+        val_4: 0
+    },
+    computed: {
+        sum: function () {
+            return this.val_1 + this.val_2 + this.val_3 + this.val_4;
+        }
     },
     beforeCreate : function(){
         console.log('Before Create');
